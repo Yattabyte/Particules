c377c9c1aa9fd00cbe194b1f0f4684bce6c76448
Yattabyte <tklowry93@gmail.com>
2020-04-19 10:45:02 -0500

    0.0.1
    Minimal compilable version with no features

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,6 @@
 #include "Utility/shader.hpp"
 #include "Utility/vec.hpp"
 #include "Utility/window.hpp"
-#include "hull.hpp"
-#include "mat.hpp"
 #include <GLFW/glfw3.h>
 #include <algorithm>
 #include <cmath>
@@ -44,63 +42,6 @@ void error_shutdown(const std::string& errorMsg) {
     std::cout << errorMsg;
     glfwTerminate();
     exit(-1);
-}
-
-void render_loop_func(
-    const double& deltaTime, double& rotation, const Shader& shader,
-    const Model& hullModel, const IndirectDraw& hullDraw,
-    const Model& cloudModel, const IndirectDraw& cloudDraw) noexcept {
-    // Update rotation based on deltaTime
-    rotation += deltaTime * 2.5F;
-
-    // Calculate viewing perspective and matrices
-    constexpr auto distance = 16.0F;
-    constexpr auto math_pi = 3.14159F;
-    const auto pMatrix = mat4::perspective(1.5708F, 1.0F, 0.01F, 10.0F);
-    const auto vMatrix = mat4::lookAt(
-        vec3{ distance * sinf(static_cast<float>(rotation) / math_pi), 0,
-              distance * cosf(static_cast<float>(rotation) / math_pi) },
-        vec3{ 0, 0, 0 }, vec3{ 0, 1, 0 });
-    const auto mMatrix = mat4();
-
-    // Flush buffers and set starting parameters
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-    glDepthFunc(GL_LEQUAL);
-
-    // Draw Gray Hull Back-face
-    glBlendFunc(GL_ONE, GL_ZERO);
-    shader.uniformLocation(0, pMatrix);
-    shader.uniformLocation(4, vMatrix);
-    shader.uniformLocation(8, mMatrix);
-    shader.uniformLocation(12, vec4{ 0.25F });
-    shader.bind();
-    hullModel.bind();
-    hullDraw.drawCall(GL_TRIANGLES);
-
-    // Draw internal point cloud model
-    glDepthFunc(GL_ALWAYS);
-    shader.uniformLocation(12, vec4{ 1, 0.25F, 0.25F, 1 });
-    cloudModel.bind();
-    cloudDraw.drawCall(GL_POINTS);
-
-    // Draw Hull White front-face
-    glDepthFunc(GL_LEQUAL);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-    shader.uniformLocation(12, vec4{ 1, 1, 1, 0.25F });
-    hullModel.bind();
-    hullDraw.drawCall(GL_TRIANGLES);
-
-    // Draw triangle outline
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    shader.uniformLocation(12, vec4{ 0.2F, 0.5F, 1, 1 });
-    hullDraw.drawCall(GL_TRIANGLES);
-
-    // Draw outside points
-    shader.uniformLocation(12, vec4{ 0.5F, 1, 0.2F, 1 });
-    cloudModel.bind();
-    cloudDraw.drawCall(GL_POINTS);
 }
 
 void register_debug() {
@@ -222,18 +163,6 @@ int main() {
         if (!shader.valid())
             error_shutdown(shader.errorLog());
 
-        // Make models
-        const auto seed(static_cast<unsigned int>(glfwGetTime()));
-        const auto pointCloud(Hull::generate_point_cloud(7.5F, 512, seed));
-        const Model hullModel(Hull::generate_convex_hull(pointCloud));
-        const Model cloudModel(pointCloud);
-        const IndirectDraw hullDraw(
-            static_cast<GLuint>(hullModel.vertexCount()), 1, 0,
-            GL_CLIENT_STORAGE_BIT);
-        const IndirectDraw cloudDraw(
-            static_cast<GLuint>(cloudModel.vertexCount()), 1, 0,
-            GL_CLIENT_STORAGE_BIT);
-
         // Enable point rendering and blending
         glEnable(GL_PROGRAM_POINT_SIZE);
         glEnable(GL_LINE_SMOOTH);
@@ -244,13 +173,11 @@ int main() {
 
         // Main Loop
         double lastTime(0.0);
-        double rotation(0.0);
         while (glfwWindowShouldClose(window.pointer()) == 0) {
             const auto time = glfwGetTime();
             const auto deltaTime = time - lastTime;
-            render_loop_func(
-                deltaTime, rotation, shader, hullModel, hullDraw, cloudModel,
-                cloudDraw);
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
             lastTime = time;
             glfwPollEvents();
             glfwSwapBuffers(window.pointer());
